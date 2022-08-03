95c4b545a367475d0b7994fcd962ff0c5d900c14
deepikatsb <deepikatsb@gmail.com>
2022-08-03 18:14:33 -0400

    NewDeepikaCommint

--- src/test/java/steps/LoginStepDefinition.java
+++ src/test/java/steps/LoginStepDefinition.java
@@ -3,9 +3,6 @@ package steps;
 import java.util.Random;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 import cucumber.api.java.After;
